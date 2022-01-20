9810adc6ab222712766f69cf9a2bb2134a734b5c
Carles01 <caguilarfernandez.cf@iesesteveterradas.cat>
2022-01-20 14:20:43 +0100

    program python files
    Update de los archivos del programa

--- M3/functions.py
+++ M3/functions.py
@@ -1,4 +1,5 @@
 import pymysql
+from datetime import datetime
 
 conn = pymysql.connect(host='51.103.33.209',user = 'bac', password='root', db='BAC')
 
@@ -47,7 +48,7 @@ def checkUser(user):
             print("The username has to be alfanumeric")
         except ZeroDivisionError:
             print("The length of username has to be longer than 5 and shorter than 11")
-    print(user)
+    return user
 
 def checkPassword(password):
     while True:
@@ -96,20 +97,22 @@ def checkPassword(password):
             print("The password have to contain some digit")
         except ZeroDivisionError:
             print("The length of password is not correct")
-    print(password)
-
+    return password
+    
 def getHeadeForTableFromTuples(t_name_columns,t_size_columns,title=""):
     num = (105 - len(title))/2
     if num % 2 == 0:
         num = int(num)
         print('='*num + title + '='*num)
+        print()
     else:
         num = int(num)
         print('='*num + title + '='*(num+1))
+        print()
         
-    print('='*105)
     print(t_name_columns[0].ljust(t_size_columns[0]),t_name_columns[1].ljust(t_size_columns[1]),t_name_columns[2].ljust(t_size_columns[2]))
-
+    print('*'*105)
+    
 def formatText(text, lenLine, split):
     if len(text) > lenLine:
         text2=""
@@ -152,41 +155,41 @@ def get_adventures_with_chars():
     return adventures
 
 def get_characters():
-    query2='select * from player' 
+    query='select * from player' 
 
-    cur.execute(query2)
+    cur.execute(query)
 
-    rows2=cur.fetchall()
+    rows=cur.fetchall()
 
     characters={}
 
-    for i in rows2:
+    for i in rows:
         characters[i[0]]=i[1]
     return characters
 
 def get_answers_bystep_adventure(aventura_escogida):
     
-    query3=f'select * from option_step where id_adventure= {str(aventura_escogida)}'
+    query=f'select * from option_step where id_adventure= {str(aventura_escogida)}'
 
-    cur.execute(query3)
+    cur.execute(query)
 
-    rows3=cur.fetchall()
+    rows=cur.fetchall()
 
     op_step={}
-    for i in rows3:
+    for i in rows:
         op_step[i[0],i[2]]={"Description":i[5],"Answer":i[4],"NextStep":i[1]}
     return op_step
 
 def getReplayAdventure():
-    query6='select g.id_game, g.date, u.id_user, u.username, a.id_adventure, a.name_adventure, p.id_player, p.name_player from game g inner join adventure a on a.id_adventure=g.id_adventure inner join player p on p.id_player=g.id_player inner join user u on u.id_user=g.id_user;'
+    query='select g.id_game, g.date, u.id_user, u.username, a.id_adventure, a.name_adventure, p.id_player, p.name_player from game g inner join adventure a on a.id_adventure=g.id_adventure inner join player p on p.id_player=g.id_player inner join user u on u.id_user=g.id_user;'
 
-    cur.execute(query6)
+    cur.execute(query)
 
-    rows6=cur.fetchall()
+    rows=cur.fetchall()
 
     replayAdventure={}
 
-    for i in rows6:
+    for i in rows:
         replayAdventure[i[0]]={'IdUser':i[2],'Username':i[3],'idAdventure':i[4],'Name':i[5],'date':i[1],'idPlayer':i[6],'PlayerName':i[7]}
     
     return replayAdventure
@@ -194,27 +197,146 @@ def getReplayAdventure():
 def get_id_bystep_adventure(aventura_escogida):
     id_step={}
 
-    query4=f"select * from step where id_adventure= {str(aventura_escogida)}"
+    query=f"select * from step where id_adventure= {str(aventura_escogida)}"
 
 
-    cur.execute(query4)
-    rows4=cur.fetchall()
+    cur.execute(query)
+
+    rows=cur.fetchall()
 
 
     listagrandepasos=[]
-    for i in range(1,len(rows4)+1):
-        query5="select s.*,o.next_step from step s inner join option_step o on s.id_step=o.current_step where o.current_step="+str(i)
-        cur.execute(query5)
-        rows5=cur.fetchall()
+    for i in range(1,len(rows)+1):
+        query1="select s.*,o.next_step from step s inner join option_step o on s.id_step=o.current_step where o.current_step="+str(i)
+        cur.execute(query1)
+        rows1=cur.fetchall()
         listapasos=[]
-        for i in rows5:
+        for i in rows1:
             listapasos.append(i[8])
         listagrandepasos.append(listapasos)
     x=0
-    for i in rows4: 
+    for i in rows: 
         id_step[i[0]]={}
         id_step[i[0]]["Description"]=i[3]
         id_step[i[0]]["Answers_Step"]=tuple(listagrandepasos[x])
         id_step[i[0]]["Final_Step"]=str(i[2])[-2]
         x+=1
     return id_step
+
+def get_first_step_adventure(aventura_escogida):
+    query="select * from step where id_adventure="+str(aventura_escogida)
+    cur.execute(query)
+    rows=cur.fetchone()
+    return rows
+
+def getUsers():
+    user={}
+    query="select * from user"
+    cur.execute(query)
+    rows=cur.fetchall()
+    for i in rows:
+        user[i[1]]={}
+        user[i[1]]["password"]=i[2]
+        user[i[1]]["idUser"]=i[0]
+    return user
+
+def UserIds():
+    name_id = []
+    username = []
+    ids = []
+    username_query = 'select username from user'
+    id_query = 'select id_user from user'
+
+    cur.execute(username_query)
+
+    rows = cur.fetchall()
+    
+    for i in rows:
+        username.append(i[0])
+
+    cur.execute(id_query)
+
+    rows = cur.fetchall()
+
+    for i in rows:
+        ids.append(i[0])
+        
+    name_id.append(username)
+    name_id.append(ids)
+
+    return name_id
+
+def insertCurrentGame(usuario,player,aventura_escogida, date,date_creation, user_creation):
+
+    query=f"insert into game (id_user,id_player,id_adventure,date,date_creation,user_creation) values ('{usuario}','{player}','{aventura_escogida}','{date}','{date_creation}','{user_creation}')"
+    cur.execute(query)
+    conn.commit()
+
+def get_table(query):
+    lista=[]
+    tuplagrande=[]
+    cur.execute(query)
+    columnas=cur.description
+    filas=cur.fetchall()
+    for i in columnas:
+        lista.append(i[0])
+    tuplagrande.append(tuple(lista))
+    tuplagrande.append(filas)
+    tuplagrande=tuple(tuplagrande)
+    return tuplagrande
+
+def getIdGames():            
+    query='select id_game from game'
+    cur.execute(query)
+    rows=cur.fetchall()
+    return rows
+
+def userExists(user):
+    for i in getUsers():
+        if user in getUsers():
+            return True
+        else:
+            return False
+            
+def getChoices(game):
+    query=f"select current_step, next_step from option_step o inner join saved_from_adventure s on o.id_option_step=s.id_option_step where id_game={str(game)}"
+    cur.execute(query)
+    row=cur.fetchall()
+    return row
+
+def insertCurrentChoice(current_step, aventura_escogida,next_step,date_creation,user_creation):
+    if next_step>=1:
+        query=f"insert into saved_from_adventure (id_game, id_step, id_adventure, id_option_step, date_creation, user_creation) values ('{getIdGames()[-1][0]}','{current_step}','{aventura_escogida}','{next_step}','{date_creation}','{user_creation}')"
+        cur.execute(query)
+        conn.commit()
+    elif next_step==0:
+        query=f"insert into saved_from_adventure (id_game, id_step, id_adventure, id_option_step, date_creation, user_creation) values ('{getIdGames()[-1][0]}','{current_step}','{aventura_escogida}',NULL,'{date_creation}','{user_creation}')"
+        cur.execute(query)
+        conn.commit()
+
+def Checkbdd(user, password):
+    userfail = 0
+    passfail = 1
+    query = 'select username, password from user'
+    cur.execute(query)
+    login = cur.fetchall()
+    data = (user, password)
+    if data in login:
+        return 1
+    else:
+        for i in login:
+            if i[0] != user:
+                userfail += 1
+            if i[1] != password:
+                passfail += 1
+        if userfail == len(login):
+            return 0
+    return -1  
+
+def setIdGames(user,aventura_escogida, player):
+    date=datetime.now()
+    id_user=getUsers()[user]['idUser']
+    query=f"insert into game (id_user, id_player, id_adventure, date, date_creation, user_creation) values ('{id_user}','{player}','{aventura_escogida}','{date}','{date}','{user}')"
+    cur.execute(query)
+    conn.commit()
+
--- M3/main.py
+++ M3/main.py
@@ -1,7 +1,23 @@
+# IMPORTS
+
 import functions
+from datetime import datetime
 
+# MENUS Y VARIABLES GLOBALES
 
-print('''
+mainmenu="\n\n1)Login\n2)Create user\n3)Replay Adventure\n4)Reports\n5)Exit"
+menu2='\n\n1)Logout\n2)Play\n3)Replay Adventure\n4)Report'
+list5=[1,2,3,4,5]
+option="\nOption: "
+
+# FLAGS PARA LOS MENUS
+
+flag0 = True
+flag2 = False
+
+while True:
+
+    print('''
 *********   *           *   **********   *********                            *********     *       *
 *           *           *   *            *                                        *         *       *
 *           *           *   *            *                                        *         *       *
@@ -40,20 +56,15 @@ print('''
 *       *          * *          *           *      **       *       *       *   *      *    *       *
 *       *           *           *********   *       *       *       *********   *       *   *       *''')
 
-menu="\n\n1)Login\n2)Create user\n3)Replay Adventure\n4)Reports\n5)Exit"
-
-option="\nOption: "
-
-lista=[1,2,3,4,5]
-opt = functions.getOpt(menu,option,lista)
-    
-if opt == 1:
-    print('opcion 1')
-elif opt == 2:
-    print('opcion 2')
-elif opt == 3:
-    print('opcion 3')
-elif opt == 4:
-    print('opcion 4')
-else:
-    print("Colorin colorado el programa se ha acabado")
+    opt = functions.getOpt(mainmenu,option,list5)
+        
+    if opt == 1:
+        
+    elif opt == 2:
+        print('opcion 2')
+    elif opt == 3:
+        print('opcion 3')
+    elif opt == 4:
+        print('opcion 4')
+    else:
+        print("Colorin colorado el programa se ha acabado")
